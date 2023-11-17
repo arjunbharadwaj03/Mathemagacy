e5c83f552c1214f6abff89c5928c1559618037ba
arjunbharadwaj03 <arjunbharadwaj03@gmail.com>
2023-11-17 21:24:17 +0530

    adding magic squares

---
+++ magicSquare.js
@@ -0,0 +1,6 @@
+const day = document.getElementById("day");
+const month = document.getElementById("month");
+const year = document.getElementById("year");
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click", () => {});
